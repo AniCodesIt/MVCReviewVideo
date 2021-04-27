2aa3ff37e58c4743dda8c3e8b2bf522c03fbd023
anicodesit <ani.kulka@gmail.com>
2021-04-26 22:24:44 -0400

    Add project files.

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var student = db.Students.Add(new Student
+            { });
+            db.SaveChanges();
+            var course = db.Courses.Add(new Course
+            { }
+            );
+            course.Students.Add();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Services/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    //business logic - all functionality lives here
+    public class Class1
+    {
+    }
+}
